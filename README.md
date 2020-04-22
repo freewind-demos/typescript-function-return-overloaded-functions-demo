a76140f713c427979c840c0fa6d51921d35a20bf
Freewind <nowindlee@gmail.com>
2020-04-22 21:11:48 +0800

    typescript-function-return-overloaded-functions-demo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-TypeScript Class Overload Methods Demo
+TypeScript Function Return Overloaded Functions Demo
 ===========================
+
+可惜的是，当写的key有误时，虽然typescript能提示编译错误，但webstorm却不能提示正确可用的值
 
 ```
 npm install
--- hello.ts
+++ hello.ts
@@ -1,15 +1,37 @@
-class User {
-  hello(name: number): void;
-  hello(name: string): void;
-  hello(name: number | string) {
-    if (typeof name === 'string') {
-      console.log(`Hello, ${name} (string)`);
-      return;
-    }
-    console.log(`Hello, ${name} (number)`);
+function buildKeys<T>(): {
+  <P1 extends keyof NonNullable<T>>(prop1: P1): string;
+  <P1 extends keyof NonNullable<T>, P2 extends keyof NonNullable<NonNullable<T>[P1]>>(prop1: P1, prop2: P2): string;
+  <P1 extends keyof NonNullable<T>, P2 extends keyof NonNullable<NonNullable<T>[P1]>, P3 extends keyof NonNullable<NonNullable<NonNullable<T>[P1]>[P2]>>(prop1: P1, prop2: P2, prop3: P3): string
+} {
+  return (...nestedKeys: string[]): string => {
+    return nestedKeys.join('.');
   }
 }
 
-const user = new User();
-user.hello('aaa');
-user.hello(111);
+type State = {
+  aaa: {
+    bbb?: {
+      ccc: string,
+      ddd: string
+    }
+  }
+}
+
+const keys = buildKeys<State>()
+const keyPath = keys('aaa', 'bbb', 'ccc')
+
+console.log(keyPath);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
